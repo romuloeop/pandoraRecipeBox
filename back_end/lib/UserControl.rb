92de555c25c4d68c4c398c372e20dcdb567dacc7
Salvador <chavo45@hotmail.com>
2016-01-08 18:12:34 -0600

    Funciones de pregunta para la clase de usuario

--- back_end/lib/UserControl.rb
+++ back_end/lib/UserControl.rb
@@ -5,21 +5,44 @@ require_relative 'DBHandler.rb';
 
 class User
 	@USERTABLE = 'users';
-	def getUserList
+	def self.getUserList
 		return DB::ask('Select username from '+@USERTABLE);
 	end
-	def self.userExist username
-		sql = "Select count(*) from "+@USERTABLE+" where username=?";
+	def self.isUser? username
+		sql = "Select count(user_id) from "+@USERTABLE+" where username=?";
 		user = DB::seq_query(sql,[username]);
 		return true if (user['count'].to_i>0);
 		return false;
 	end
-	private :getUserList;
+	def self.isAdmin? username
+		sql = "Select admin from "+@USERTABLE+" where username=?";
+		user = DB::seq_query(sql, [username]);
+		print user
+	end
+	def self.isActive? username
+		sql = "Select active from "+@USERTABLE+" where username=?";
+		user = DB::seq_query(sql, [username]);
+		return user if(user === nil)
+		return (user[0]['active']=='true')
+	end
+	def self.isBlocked? username
+		sql = "Select blocked from "+@USERTABLE+" where username=?";
+		user=  DB::seq_query(sql, [username]);
+		puts user;
+	end
+	def self.isValid?(username,password)
+		sql = "Select count(user_id) from "+@USERTABLE+" where active and not blocked and username=? and ssap=md5(?);";
+		user = DB::seq_query(sql,[username, password]);
+		return true if (user['count'].to_i>0);
+		return false;
+	end
+	private_class_method :getUserList;
 	#public :userExist?;
 end
 
 User.new;
 #puts User::@USERTABLE;
-puts User.userExist 'pedroparamo';
+#puts User.isValid? 'pedroparamo','1234';
+puts User.isActive? 'pedroparamo';
 
 
